c6108714d996a36a474ea0d820a553658789414f
adocal5353 <calebadogli53@gmail.com>
2023-08-15 18:29:58 +0000

    modifying docView

--- lib/app/UI/views/homeView/home_view.dart
+++ lib/app/UI/views/homeView/home_view.dart
@@ -1,72 +1,151 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:my_docs/models/Doc.dart';
+import 'package:get/get.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:lottie/lottie.dart';
+import 'package:my_docs/config/config.dart';
+import 'package:my_docs/controllers/document_controller.dart';
+import 'package:my_docs/models/document.dart';
 import 'package:my_docs/widgets/docView.dart';
+import 'package:my_docs/app/services/dio_service.dart';
 
-class HomeView extends StatefulWidget {
-  const HomeView({Key? key}) : super(key: key);
+class HomeView extends StatelessWidget {
+  HomeView({Key? key}) : super(key: key);
 
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  var containerRaduis = Radius.circular(30.0);
+  final Radius containerRaduis = const Radius.circular(30);
+  DocumentController documentController = Get.put(DocumentController());
+  var documents = [
+    new Document("evefv", 1, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 2, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 3, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 4, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 5, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 6, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 7, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 8, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 9, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 10, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100),
+    new Document("evefv", 11, "Caleb", "Good news",
+        "La bonne nouvelle doit être propagé dans tout le monde", 100)
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topRight: containerRaduis,
-          topLeft: containerRaduis,
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 225, 218, 218),
-                    hintText: "Chercher un document...",
-                    hintStyle: TextStyle(
-                        color: Color.fromARGB(255, 129, 123, 123),
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w300),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
+    return SizedBox(
+      height: double.maxFinite,
+      width: double.maxFinite,
+      child: Column(
+        children: [
+          SizedBox(
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color.fromARGB(255, 251, 240, 240),
+                hintText: 'Chercher un document par le titre...',
+                hintStyle: const TextStyle(
+                  color: Color.fromARGB(255, 129, 123, 123),
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(50),
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              ListView(
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                children: [
-                  docView("caleb.pdf",
-                      "Bibliographie de caleb, qui porte sur les routes et les middelware"),
-                  docView("une vie de boy"),
-                  docView("Les 100 vivants"),
-                  docView("Faire de son mieux"),
-                  docView("Père riche, père pauvre"),
-                  docView("Le prophète et sa mission"),
-                  docView("Le doigt de DIEU"),
-                  docView("Bonjour, Saint Esprit"),
-                  docView("Accomplir son ministère"),
-                  docView("Les lois de la richesse"),
-                  docView("Le soleil des indépendances et des régimes forcés"),
-                  docView("Sous l'orage", "Séydou Badian"),
-                ],
-              ),
-            ],
+            ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: () {
+                return documentController.getDocuments();
+              },
+              color: Colors.deepPurpleAccent,
+              child: GetBuilder<DocumentController>(
+                id: 1,
+                builder: (_) => SingleChildScrollView(
+                  child: !documentController.isInternetConnect.value
+                      ? Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height: 150,
+                                width: 150,
+                                child: Lottie.asset(
+                                    'assets/animations/no-internet.json'),
+                              ),
+                              MaterialButton(
+                                onPressed: () async {
+                                  if (await InternetConnectionChecker()
+                                          .hasConnection ==
+                                      true) {
+                                    documentController.getDocuments();
+                                  } else {
+                                    showCustomSnackBar(context,
+                                        "Erreur, Veuillez vérifier votre connexion internet.");
+                                  }
+                                },
+                                color: Colors.red.shade300,
+                                child: Text(
+                                  "Réessayer",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 12),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      : documentController.isLoading.value
+                          ? _buildLoading()
+                          : _buildBody(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView.separated(
+      itemCount: documents.length,
+      itemBuilder: (context, index) {
+        return docWait(documents[index]);
+      },
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      separatorBuilder: (context, index) {
+        return SizedBox(
+          height: 8,
+          
+        );
+      },
+    );
+  }
+
+  Center _buildLoading() {
+    return Center(
+      child: SizedBox(
+        height: 150,
+        width: 150,
+        child: Lottie.asset(
+          'assets/animations/docloader.json',
         ),
       ),
     );
